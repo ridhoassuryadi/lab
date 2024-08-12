2b01d906ec5e88e6c4caf623dae61c5b7c280c5c
ridho <ulunmuhammadridho@gmail.com>
2024-08-12 08:26:10 +0800

    add kalseljs

---
+++ app/src/Event.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import CodeEditor from '@uiw/react-textarea-code-editor';
+
+export function Event() {
+  return (
+    <CodeEditor
+      value={    `function add(a, b) {\n  return a + b;\n}`}
+      language="js"
+      placeholder="Please enter JS code."
+      padding={15}
+      style={{
+        backgroundColor: "#f5f5f5",
+        fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+      }}
+    />
+  );
+}
