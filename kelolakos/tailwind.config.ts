7d7e8b5bc7c2d242e51b9201e8bfb22751bc7183
Ridho <mridho@ruangguru.com>
2024-08-16 11:29:42 +0700

    animated beam

--- kelolakos/tailwind.config.ts
+++ kelolakos/tailwind.config.ts
@@ -1,66 +1,80 @@
-import { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
-  darkMode: "media",
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      width: {
-        "content-lg": "560px",
-        content: "90vw",
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      media: {
-        xs: "430px",
-        xxs: "375px",
-      },
-      animation: {
-        img: "img 1s ease-in-out",
-        intro: "intro 0.3s forwards ease-in-out",
-        marker: "marker 4s ease-out infinite",
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        img: {
-          from: { transform: "scale(2)", filter: "blur(10px)" },
-          to: { transform: "scale(1)", filter: "blur(0px)" },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        marker: {
-          "0%": {
-            transform: "translate(-50%, -50%) scale(1)",
-            opacity: "1",
-          },
-          "35%": {
-            transform: "translate(-50%, -50%) scale(6)",
-            opacity: "0",
-          },
-          "100%": {
-            transform: "translate(-50%, -50%) scale(6)",
-            opacity: "0",
-          },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
-        intro: {
-          "0%": {
-            transform: "translateY(10px)",
-            opacity: "0",
-            filter: "blur(5px)",
-          },
-          "95%": {
-            transform: "translateY(-1px)",
-            opacity: "1",
-            filter: "blur(0px)",
-          },
-          "100%": {
-            transform: "translateY(0px)",
-            opacity: "1",
-            filter: "blur(0px)",
-          },
-        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
